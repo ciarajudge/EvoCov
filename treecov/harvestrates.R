5dd22d86b6cce9f247f6db53b9e6eb9a65800b8b
Ciara Judge <judge.ciara@gmail.com>
2021-07-28 19:55:29 +0100

    fixing a teeny bug

--- treecov/harvestrates.R
+++ treecov/harvestrates.R
@@ -5,9 +5,9 @@ if (file.exists("rates.csv")){
   rates <- read.table("rates", sep = "", skip = 9, header = F, fill = TRUE)
   sitewiserates <- rates[1:3822,4]
   combined <- cbind(master, sitewiserates)
-  write.csv(combined, "rates.csv")
+  write.csv(combined, "rates.csv", row.names = F, col.names = F)
 } else {
   rates <- read.table("rates", sep = "", skip = 9, header = F, fill = TRUE)
   combined <- cbind(rates[1:3822,1], rates[1:3822,4])
-  write.csv(combined, "rates.csv")
+  write.csv(combined, "rates.csv", row.names = F, col.names = F)
 }
